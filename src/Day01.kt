69d2e2b8805d8a9e18f3d8a7485f5efd0d96d127
Frank Harper <frank@franklinharper.com>
2025-12-01 21:33:54 -0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,64 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var pos = 50
+        var res = 0
+        for (s in input) {
+            val value = s.substring(1).toInt()
+            val offset = if (s.startsWith("L")) -value else value
+            pos = (pos + offset).mod(100)
+            println(pos)
+            if (pos == 0) res++
+        }
+        println("///")
+        return res
+    }
+
+    fun countZeros(pos: Int, s: String): Pair<Int, Int> {
+        var newPos = pos
+        var zeroCount = 0
+        val offset = if (s.startsWith("L")) -1 else 1
+        val value = s.substring(1).toInt()
+        repeat(times = value) {
+            newPos += offset
+            if (newPos == -1) newPos = 99
+            if (newPos == 100) newPos = 0
+            if (newPos == 0) zeroCount++
+//            println(newPos)
+        }
+        println("///")
+        return Pair(newPos, zeroCount)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var pos = 50
+        var zeroCount = 0
+        for (s in input) {
+            val (newPos, zeros) = countZeros(pos, s)
+            pos = newPos
+            println(pos)
+            zeroCount += zeros
+        }
+        println("***")
+        return zeroCount
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("Test part1")
+    checkEquals(
+        actual = part1(testInput),
+        expected = 3
+    )
+
+    println("Test part2")
+    checkEquals(
+        actual = part2(testInput),
+        expected = 6
+    )
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day01_input")
+    println("Result part1")
     part1(input).println()
+    println("Result part2")
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T> checkEquals(actual: T, expected: T, message: String = "") {
+    check(actual == expected) { "Expected $expected, but found $actual. $message" }
+}
