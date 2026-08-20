a689cebd6606b3ae16dc6f11639e339f101ca37e
Frank Harper <Frank.harper@gmail.com>
2026-08-20 14:51:28 -0700

    Add Day XX solution template

---
+++ src/DayXX.kt
@@ -0,0 +1,38 @@
+fun main() {
+
+    val day = "DayXX"
+
+    fun part1(input: List<String>): Long {
+        return 0L
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+    val testInput = readInput("${day}_test")
+
+    checkEquals(
+        message = "Test part1",
+        actual = part1(testInput),
+        expected = 0
+    )
+
+//    val input = readInput("${day}_input")
+//    println("Result part1: ${part1(input)}")
+//
+//    checkEquals(
+//        message = "Test part2",
+//        actual = part2(testInput),
+//        expected = 0
+//    )
+//
+//    val part2Result = part2(input)
+//    println("Result part2: $part2Result")
+//
+//    checkEquals(
+//        message = "Part2 result",
+//        actual = part2Result,
+//        expected = 9770311947567L
+//    )
+}
